ab079212b6e142b90724b7d8693469c1d0b77f65
maksimzhuk91 <maksim.zhuk91@gmail.com>
2019-04-20 13:45:31 +0300

    lesson 9 registration

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -6,11 +6,17 @@ import { User } from '../models/user.model';
 
 @Injectable()
 export class UsersService {
-  constructor(private http: Http) {}
+  constructor(private http: Http) {
+  }
 
   getUserByEmail(email: string): Observable<User> {
     return this.http.get(`http://localhost:3000/users?email=${email}`)
       .map((response: Response) => response.json())
       .map((user: User[]) => user[0] ? user[0] : undefined);
+  }
+
+  createNewUser(user: User): Observable<User> {
+    return this.http.post('http://localhost:3000/users', user)
+      .map((response: Response) => response.json());
   }
 }
